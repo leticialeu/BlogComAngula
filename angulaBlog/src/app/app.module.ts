c9d60ad66b31f3c055bf04cb79f87d36d2bb3f38
leticia oliveira <leticiaoliveiraju@gmail.com>
2024-08-08 17:18:46 -0300

    commit

--- angulaBlog/src/app/app.module.ts
+++ angulaBlog/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './componetes/menu-title/menu-title.component';
+import { BigCardComponent } from './componetes/big-card/big-card.component';
+import { SmalCardComponent } from './componetes/smal-card/smal-card.component';
+import { MenuBarComponent } from './componetes/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmalCardComponent,
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
